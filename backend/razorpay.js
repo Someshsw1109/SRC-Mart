fd4b986a8408b144abd13e4fe1d7463385cd8081
Somesh Raj <someshraj78569@gmail.com>
2025-01-02 11:37:31 +0530

    Update razorpay.js

--- backend/razorpay.js
+++ backend/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay = require('razorpay');
 
 const razorpayInstance = new Razorpay({
-  key_id: 'rzp_test_0tOMsiuMF3fU4v', 
-  key_secret: 'qcYspB9yhJRgBUd7fbdKOpQ0' 
+  key_id: 'rzp_live_hh9FzKxfOAinEy', 
+  key_secret: '2Q2SAcKas2n38HvAHReePvE4' 
 });
 
 module.exports = razorpayInstance;
